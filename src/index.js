d46c52fd5bc7467a1795dc3d22651acb7ead1e06
luciebarbierdearnley <barbier.lucie1@gmail.com>
2024-02-28 14:38:17 +0000

    added delete function

--- src/index.js
+++ src/index.js
@@ -4,9 +4,19 @@ function addToList(event) {
   let listItems = document.querySelector("#items");
 
   listItems.innerHTML += ` <div class="display-items">
-            <li>${enteredItem}s</li>
-            <p class="bg-lime-600 ps-4 pe-4 rounded-full text-white add">x</p>
+            <li id="delete-item">${enteredItem}s</li>
+            <button class="bg-lime-600 ps-4 pe-4 rounded-full text-white add" id="delete-button">x</button>
           </div>`;
+
+  let deleteAction = document.getElementById("delete-button");
+  deleteAction.addEventListener("click", deleteItem);
+}
+
+function deleteItem(event) {
+  let deleteButton = document.getElementById("delete-button");
+  let deleteItem = document.getElementById("delete-item");
+  deleteButton.remove();
+  deleteItem.remove();
 }
 
 let addButtonEl = document.querySelector("#form");
