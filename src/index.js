8f381bbcf593d2aedfda062bb39e07094cc03b2d
luciebarbierdearnley <barbier.lucie1@gmail.com>
2024-02-28 11:27:34 +0000

    Added JS list creation

--- src/index.js
+++ src/index.js
@@ -3,7 +3,20 @@ function addToList(event) {
   let enteredItem = document.querySelector("#user-input").value;
   let listItems = document.querySelector("#items");
 
-  listItems.innerHTML = enteredItem;
+  listItems.innerHTML += ` <div class="display-items">
+            <li>${enteredItem}s</li>
+            <p class="bg-lime-600 ps-4 pe-4 rounded-full text-white add">x</p>
+          </div>`;
+
+  /*let listItemsEl = document.createElement("li");
+  listItemsEl.textContent = enteredItem;
+
+  let binIcon = document.createElement("span");
+  binIcon.textContent = "x";
+  binIcon.classList.add("delete");
+
+  listItems.append(listItemsEl, binIcon);
+}*/
 }
 
 let addButtonEl = document.querySelector("#form");
